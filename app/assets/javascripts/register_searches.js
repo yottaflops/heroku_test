7cbb4eaa0fa92d46b04b47152cd09ad3dc05c03b
yottaflops <yottaflops89@gmail.com>
2014-09-21 19:28:51 -0400

    fixes whitespace and form display bug

--- app/assets/javascripts/register_searches.js
+++ app/assets/javascripts/register_searches.js
@@ -1,13 +1,12 @@
 $(document).ready(function() {
+  $("input.text").prop("checked", true)
 
-	$("input.text").prop("checked", true)
+  var displaySearchOption = function (searchOption) {
+    $('.js-search-option').hide();
+    $('.js-search-option.' + searchOption + "-search").show()
+  };
 
-	var displaySearchOption = function (searchOption) {
-		$('.js-search-option').hide();
-		$('.js-search-option.' + searchOption + "-search").show()
-	};
-
-	$('input').change(function () {
-		displaySearchOption($(this).val())
-	});	
+  $('input').change(function () {
+    displaySearchOption($("input:checked").val())
+  });
 });
