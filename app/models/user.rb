16692aab9741628cd1617c61b31db840dab23c73
yottaflops <yottaflops89@gmail.com>
2014-10-11 18:06:06 -0400

    configures devise validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,13 @@
 class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
+
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
+         :recoverable, :rememberable, :trackable
   has_many :register_searches, inverse_of: :user, dependent: :destroy
+
+  validates_uniqueness_of :email, case_sensitive: false, allow_blank: true, if: :email_changed?
+  validates_format_of :email, with: Devise.email_regexp, allow_blank: true, if: :email_changed?
+  validates_presence_of :password, on: :create
+  validates_confirmation_of :password, on: :create
 end
